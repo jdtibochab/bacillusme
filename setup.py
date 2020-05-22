3185e39e8e8a988c2a4918e91449e86e80c4d165
jdtibochab <jdtibochab@gmail.com>
2020-05-22 11:46:59 -0700

    checkpoint

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 
-setup(name='ECOLIme',
+setup(name='BACILLUSme',
       version='0.0.9',
-      description='ME-model data files specific to E. coli K-12 MG1655',
-      author='Colton Lloyd and Ali Ebrahim',
-      url='https://github.com/SBRG/ecolime',
+      description='ME-model data files specific to Bacillus subtilis str. 168',
+      author='Juan D. Tibocha-Bonilla',
+      url='https://github.com/jdtibochab/bacillusme',
       install_requires=['Biopython', 'setuptools', 'cobra<=0.5.11', 'xlrd',
                         'pandas', 'six', 'scipy', 'numpy'],
       packages=find_packages(),
--- travis_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -v
-
-pip install git+https://github.com/${TRAVIS_REPO_SLUG%/*}/cobrame.git@${TRAVIS_BRANCH}
